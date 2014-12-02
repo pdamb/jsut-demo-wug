26edc74db69bf06a2bf9f5128f42227430e40d34
unknown <Petr.Damborsky@PDAMBORSKY-LT.tul.solarwinds.net>
2014-12-02 18:14:07 +0100

    Adding tests

--- examples/tests/ex1.ChatTests.js
+++ examples/tests/ex1.ChatTests.js
@@ -1,1 +1,27 @@
-﻿
+﻿/// <reference path="../js/ex1.Chat.js" />
+
+module('zakladni testy', {
+    setup: function() {
+        this.c = new ns.Chat();
+    },
+    teardown: function() {
+        delete this.c;
+    }
+});
+
+test('Zprava "jsi skvely" je v poradku.', function (assert) {
+    var zprava = 'jsi skvely';
+    var vysledek = this.c.validate(zprava);
+    assert.equal(vysledek, true, 'vse bylo ok');
+});
+
+test('zprava "jsi noob" neni v poradku.', function(assert) {
+    var zprava = 'jsi noob';
+    var vysledek = this.c.validate(zprava);
+    assert.ok(!vysledek);
+});
+
+module('abc');
+test('zprava "jsi noob" neni v poradku.', function (assert) {
+    assert.ok(1);
+});
--- examples/tests/ex2.AsyncChatTests.js
+++ examples/tests/ex2.AsyncChatTests.js
@@ -1,1 +1,18 @@
-﻿
+﻿/// <reference path="../lib/jquery-1.11.1.min.js" />
+/// <reference path="../lib/jquery.mockjax.js" />
+/// <reference path="../js/ex2.AsyncChat.js" />
+
+asyncTest('Zprava "jsi noob" neni validni.', function(assert) {
+    expect(1);
+    var zprava = 'jsi noob';
+    var chat = new ns.AsyncChat();
+    chat.validate(zprava, function(vysledek) {
+        assert.equal(vysledek, false);
+    });
+    QUnit.start();
+});
+
+asyncTest('Zprava "jsi noob" neni validni.', function (assert) {
+    assert.ok(1);
+    QUnit.start();
+});
